d076d4a5633fd65ac0ed2948e990a1374757750a
purin-kan <purinkk9@gmail.com>
2023-11-02 11:17:01 +0700

    fixed lab8 q3

--- Term1/Post Midterm/lab8/q3.py
+++ Term1/Post Midterm/lab8/q3.py
@@ -1,7 +1,19 @@
-seq1 = set(input("Input sequence1: ").split())
-seq2 = set(input("Input sequence2: ").split())
+seq1 = input("Input sequence1: ").split()
+seq2 = input("Input sequence2: ").split()
 
-if seq1 == seq2:
+set1 = set()
+set2 = set()
+
+for i in seq1:
+    if i.isdigit(): 
+        set1.add(i)
+
+for j in seq2:
+    if j.isdigit(): 
+        set2.add(j)
+    
+if set1 == set2:
     print("Output: same set")
-else: 
+else:
     print("Output: different set")
+    
